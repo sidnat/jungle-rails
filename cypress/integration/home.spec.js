cea2e864ba1426047f3d3f77f77ec87423979746
Sid Natarajan <sid.nat@gmail.com>
2023-01-05 13:27:49 +0000

    Feature Spec: Home Page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Jungle rails app', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
